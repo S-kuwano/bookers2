4366b7fe93131ca22c2428226ad84a3e0cbc63cb
S-kuwano <tukushi14@gmail.com>
2021-06-20 13:00:08 +0000

    [Update]210620

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,31 @@
 class BooksController < ApplicationController
+
+
+  def index
+    @users = User.all
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user.id = current_user.id
+    @book.save
+    redirect_to books_path
+  end
+
+
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def destroy
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :opinion)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/show'
+  root 'homes#top'
+  get 'about' => 'homes#'
   devise_for :users
+  resources :books, only: [:create, :index, :show, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show, :index]
 end
