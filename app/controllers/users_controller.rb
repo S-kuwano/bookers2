900b8e69fb718eab17630d444f8dccad22423293
S-kuwano <tukushi14@gmail.com>
2021-06-21 14:57:40 +0000

    [Update]210621

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,15 @@ class UsersController < ApplicationController
   end
 
   def index
+    @user = current_user
+    @book = Book.new
+    @books = Book.all
     @users = User.all
   end
   
+  private
+  def user_params
+  params.require(:user).permit(:title, :body, :image)  
+  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'homes#top'
   get 'about' => 'homes#'
   devise_for :users
-  resources :books, only: [:create, :index, :show, :destroy]
+  resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:show, :index]
+  resources :users
 end
